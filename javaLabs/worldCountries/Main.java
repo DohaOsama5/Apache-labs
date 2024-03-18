338aabf555b0f1f567599a29c8cb0db7030d2800
Doha Osama <155627017+DohaOsama5@users.noreply.github.com>
2024-03-18 15:43:06 +0200

    Add files via upload

---
+++ javaLabs/worldCountries/Main.java
@@ -0,0 +1,61 @@
+package Countries;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+       
+        ArrayList<Country> countries = new ArrayList<>();
+        ArrayList<City> cities = new ArrayList<>();
+
+        countries.add(new Country("ABW", "Aruba", "North America", 103000, 193, 828, 129));
+        countries.add(new Country("AFG", "Afghanistan", "Asia", 22720000, 652090, 5976, 1));
+        countries.add(new Country("AGO", "Angola", "Africa", 12878000, 1246700, 6648, 56));
+      
+        cities.add(new City(1, "Kabul", 1780000, "AFG"));
+        cities.add(new City(2, "Qandahar", 237500, "AFG"));
+        cities.add(new City(3, "Herat", 186800, "AFG"));
+       
+        Map<String, City> highestPopulatedCities = new HashMap<>();
+        for (City city : cities) {
+            String countryCode = city.getCountryCode();
+            if (!highestPopulatedCities.containsKey(countryCode) ||
+                    city.getPopulation() > highestPopulatedCities.get(countryCode).getPopulation()) {
+                highestPopulatedCities.put(countryCode, city);
+            }
+        }
+        System.out.println("highest populated cities:");
+        highestPopulatedCities.forEach(
+                (countryCode, city) -> System.out.println("Country: " + countryCode + ", City: " + city.getName() +
+                        ", Population: " + city.getPopulation()));
+
+        Map<String, Country> mostPopulatedCountries = new HashMap<>();
+        for (Country country : countries) {
+            String continent = country.getContinent();
+            if (!mostPopulatedCountries.containsKey(continent) ||
+                    country.getPopulation() > mostPopulatedCountries.get(continent).getPopulation()) {
+                mostPopulatedCountries.put(continent, country);
+            }
+        }
+        System.out.println("\nMost Populated Countries by Continent:");
+        mostPopulatedCountries.forEach((continent, country) -> System.out
+                .println("Continent: " + continent + ", Country: " + country.getName() +
+                        ", Population: " + country.getPopulation()));
+
+        City highestPopulatedCapital = null;
+        for (City city : cities) {
+            if (city.getId() == 1 && (highestPopulatedCapital == null ||
+                    city.getPopulation() > highestPopulatedCapital.getPopulation())) {
+                highestPopulatedCapital = city;
+            }
+        }
+        System.out.println("\nHighest Populated Capital City:");
+        if (highestPopulatedCapital != null) {
+            System.out.println("City: " + highestPopulatedCapital.getName() +
+                    ", Population: " + highestPopulatedCapital.getPopulation());
+        }
+    }
+}
